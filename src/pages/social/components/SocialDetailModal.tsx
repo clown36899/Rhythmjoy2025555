3695f2e61985d37598989a9f81ca77fe4901a6ff
Intae Yeo <you@example.com>
2026-01-03 14:57:14 +0900

    지도개선

--- src/pages/social/components/SocialDetailModal.tsx
+++ src/pages/social/components/SocialDetailModal.tsx
@@ -6,6 +6,7 @@ import { useModalHistory } from '../../../hooks/useModalHistory';
 import { useAuth } from '../../../contexts/AuthContext';
 
 import { getDayName } from '../../v2/utils/eventListUtils';
+import { sanitizeAddressForMap } from '../../../utils/mapUtils';
 
 interface SocialDetailModalProps {
     isOpen: boolean;
@@ -209,11 +210,7 @@ const SocialDetailModal: React.FC<SocialDetailModalProps> = ({
 
                         <div
                             className="detail-place-box"
-                            onClick={openMap}
-                            data-analytics-id={schedule.id}
-                            data-analytics-type="map_link"
-                            data-analytics-title={schedule.place_name}
-                            data-analytics-section="social_detail_content"
+                            style={{ cursor: 'default' }} // Remove pointer cursor from container
                         >
                             <div className="place-icon">
                                 <i className="ri-map-pin-2-fill"></i>
@@ -221,9 +218,37 @@ const SocialDetailModal: React.FC<SocialDetailModalProps> = ({
                             <div className="place-info">
                                 <div className="place-name">{schedule.place_name}</div>
                                 <div className="place-addr">{schedule.address || '주소 정보가 없습니다.'}</div>
-                            </div>
-                            <div className="place-map-arrow">
-                                <i className="ri-external-link-line"></i>
+
+                                <div className="detail-map-buttons">
+                                    <button
+                                        onClick={openMap}
+                                        className="detail-map-btn naver"
+                                        data-analytics-id={schedule.id}
+                                        data-analytics-type="map_link_naver"
+                                    >
+                                        <i className="ri-map-pin-line"></i> 네이버지도
+                                    </button>
+                                    <button
+                                        onClick={() => {
+                                            if (schedule.address) {
+                                                const query = sanitizeAddressForMap(schedule.address);
+                                                window.open(`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(query)}`, '_blank');
+                                            } else if (schedule.place_name) {
+                                                window.open(`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(schedule.place_name)}`, '_blank');
+                                            }
+                                        }}
+                                        className="detail-map-btn google"
+                                        data-analytics-id={schedule.id}
+                                        data-analytics-type="map_link_google"
+                                    >
+                                        <svg width="18" height="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
+                                            <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
+                                            <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
+                                            <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" />
+                                        </svg> Google Map
+                                    </button>
+                                </div>
                             </div>
                         </div>
 
