8db20e2e6b2114de6840dd3ba77688490c530dc0
Intae Yeo <you@example.com>
2026-02-03 18:23:21 +0900

    .

--- src/pages/social/components/SocialEventModal.tsx
+++ src/pages/social/components/SocialEventModal.tsx
@@ -5,8 +5,8 @@ import { useAuth } from '../../../contexts/AuthContext';
 import { useModal } from '../../../hooks/useModal';
 import { useModalHistory } from '../../../hooks/useModalHistory';
 import VenueSelectModal from '../../v2/components/VenueSelectModal';
-import '../../../../styles/domains/events.css';
-import '../../../../styles/components/SocialEventModal.css';
+import '../../../styles/domains/events.css';
+import '../../../styles/components/SocialEventModal.css';
 
 interface SocialEventModalProps {
   onClose: () => void;
